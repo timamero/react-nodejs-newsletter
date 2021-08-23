f90b8f87d6be43085f8236353fa0f3e48c65e041
Fatima <fcamero12@gmail.com>
2021-08-23 15:14:35 -0700

    Add connection to mongodb

--- newsletter-backend/app.js
+++ newsletter-backend/app.js
@@ -5,6 +5,16 @@ const cors = require('cors')
 const middleware = require('./config/middleware')
 const articlesRouter = require('./controllers/articles')
 const emailsRouter = require('./controllers/emails')
+const mongoose = require('mongoose')
+
+console.log('connecting to ', process.env.MONGoDB_URI)
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.json())
