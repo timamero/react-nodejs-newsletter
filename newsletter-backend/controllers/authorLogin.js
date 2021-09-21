1f3b208ed63c79842d2c5ab34d2960e6b89fd2a3
Fatima <fcamero12@gmail.com>
2021-09-20 20:14:21 -0700

    Fix error handling for author login

--- newsletter-backend/controllers/authorLogin.js
+++ newsletter-backend/controllers/authorLogin.js
@@ -3,19 +3,25 @@ const bcrypt = require('bcrypt')
 const authorLoginRouter = require('express').Router()
 const AuthorUser = require('../models/authorUser')
 
-authorLoginRouter.post('/', async (request, response) => {
+authorLoginRouter.post('/', async (request, response, next) => {
   const body = request.body
 
   AuthorUser.findOne({ username: body.username })
     .then(returnedUser => {
+      if (!returnedUser) {
+        return response.status(401).json({
+          error: 'the user does not exist'
+        })
+      }
+
+      if (!returnedUser.passwordHash) {
+        return response.status(401).json({
+          error: 'invalid username or password'
+        })
+      }
+
       bcrypt.compare(body.password, returnedUser.passwordHash)
         .then(result => {
-          if (!result) {
-            return response.status(401).json({
-              error: 'invalid username or password'
-            })
-          }
-
           const authorUserForToken = {
             username: returnedUser.username,
             id: returnedUser._id
@@ -27,7 +33,9 @@ authorLoginRouter.post('/', async (request, response) => {
             .status(200)
             .send({ token, username: returnedUser.username, name: returnedUser.name })
         })
+        .catch(error => next(error))
     })
+    .catch(error => next(error))
 })
 
 module.exports = authorLoginRouter
