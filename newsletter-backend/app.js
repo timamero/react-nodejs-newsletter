39a2f03415b1dde0b220d34ca9a87377f43a08bb
Fatima Camero <fcamero12@gmail.com>
2021-11-29 13:41:21 -0800

    add static file serving

--- newsletter-backend/app.js
+++ newsletter-backend/app.js
@@ -22,6 +22,7 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 app.use('/api/articles', articlesRouter)
 app.use('/api/preview', previewRouter)
